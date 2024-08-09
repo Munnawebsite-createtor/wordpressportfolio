575fe73bd06f431672a21453c29851f4d8a210c4
munnaalways <155769662+Munnawebsite-createtor@users.noreply.github.com>
2024-08-09 23:54:30 +0600

    file aded
    file aded

---
+++ scripts.js
@@ -0,0 +1,32 @@
+
+
+let mobile_icon = document.getElementById("mobile_icon");
+let main_menu = document.getElementById("main_menu");
+mobile_icon.addEventListener("click"; () =<> {
+main_menu.classList.toggle('hidden');})
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  theme: {
+    extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
